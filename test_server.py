ecd9feeb5734392cf6c6a4fe454df4149e463bb8
Varun Kuntal <varunkuntal@yahoo.in>
2023-03-18 10:49:50 +0000

    Added test_server.py, added util get_class_names()

---
+++ test_server.py
@@ -0,0 +1,62 @@
+import requests
+import argparse
+import os
+import time
+
+import requests
+from PIL import Image
+
+from model import OnnxModel, Preprocessor
+from utils import get_class_names
+
+def get_class_names():
+    filename = "imagenet_class_names.txt"
+    try:
+        with open(filename) as f:
+            class_names = f.readlines()
+        return [c.strip() for c in class_names]
+    except FileNotFoundError:
+        url = "https://raw.githubusercontent.com/anishathalye/imagenet-simple-labels/master/imagenet-simple-labels.json"
+        response = requests.get(url)
+        class_names = response.json()
+        with open(filename, "w") as f:
+            for c in class_names:
+                f.write(c + "\n")
+        return class_names[:1000]
+
+
+def classify_image(image_path, onnx_model, preprocessor):
+    img = Image.open(image_path)
+    img_tensor = preprocessor(img)
+    output = onnx_model.predict(img_tensor)
+    class_idx = output.index(max(output))
+    class_name = get_class_names()[class_idx]
+    return class_name
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Test server for ONNX model deployment')
+    parser.add_argument('image_path', type=str, help='Path to the image to classify')
+    parser.add_argument('--test', action='store_true', help='Run preset tests')
+    args = parser.parse_args()
+
+    onnx_model = OnnxModel(os.path.join('model', 'model_optimized.onnx'))
+    preprocessor = Preprocessor()
+
+    if args.test:
+        images = [
+            ('images/n01440764_tench.jpeg', 'goldfish, Carassius auratus'),
+            ('images/n01667114_mud_turtle.JPEG', 'mud turtle'),
+        ]
+        for image_path, expected_class in images:
+            class_name = classify_image(image_path, onnx_model, preprocessor)
+            print(f'Image: {image_path}')
+            print(f'Expected Class: {expected_class}')
+            print(f'Classified As: {class_name}')
+            print('-' * 50)
+
+    start_time = time.time()
+    class_name = classify_image(args.image_path, onnx_model, preprocessor)
+    end_time = time.time()
+    print(f'Classified image as: {class_name}')
+    print(f'Time taken: {end_time - start_time:.4f}s')
